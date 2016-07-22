e4d4ab36faea7cd2f4a50081a659d2599c53bc46
Kévio Castro <keviocastro@gmail.com>
2016-07-22 17:13:03 -0300

    Test auth0 check id_token

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/schools', array('middleware' => 'auth0.jwt', function() {
+    return "Allowed access";
+}));
--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Auth0
+        'Auth0\Login\LoginServiceProvider',
+
     ],
 
     /*
@@ -201,6 +204,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Auth0' => 'Auth0\Login\Facade\Auth0',
 
     ],
 
