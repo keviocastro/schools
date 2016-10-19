5d06ffeceb1b0adb388358dea1da6d0475b691b8
Kévio Castro <keviocastro@gmail.com>
2016-10-19 17:40:28 -0200

    Seeder for lessons Config for aws environment

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM keviocastro/laravel:5
 
+# For aws environment test: "gennin"
+ENV DB_HOST="aahmh4goognvs1.cxudyan7umuk.us-east-1.rds.amazonaws.com"
+ENV DB_USERNAME="schools"
+ENV DB_PASSWORD="secretone"
+
 EXPOSE 80
---
+++ database/seeds/Lessons2016.php
@@ -0,0 +1,36 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class Lessons2016 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $start = Carbon::create(2016, 1, 10, 8);
+        $end = Carbon::create(2016, 1, 10, 8, 45);
+        $last = Carbon::create(2016, 12, 10, 8, 45);
+
+        $schoolClass = factory(App\SchoolClass::class)->create();
+
+        while ( $start->lt($last) ) {
+        	if ($start->dayOfWeek !=  Carbon::SUNDAY && 
+        		$start->dayOfWeek != Carbon::SATURDAY) {
+        		
+	        	factory(App\Lesson::class)->create([
+	        			'school_class_id' => $schoolClass->id,
+	        			'start' => $start,
+	        			'end' => $end
+	        		]);
+        	}
+
+        	$start->addDays(1);
+        	$end->addDays(1);
+        }
+    }
+}
