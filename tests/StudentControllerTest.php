afa85117c4a1c7c229057cb29331fe12ee118293
Kévio Castro <keviocastro@gmail.com>
2016-11-11 16:42:23 -0200

    Test for student summary

--- tests/StudentControllerTest.php
+++ tests/StudentControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\SchoolClassStudent;
+use App\Subject;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -25,6 +26,10 @@ class StudentControllerTest extends TestCase
     }
 
     /**
+     * @todo  Remove seeJsonEquals e criar teste para o methodo Student::annualSummary
+     *        pois é ele que tem a responsabilidade de gerar a info certa.
+     *        Nos testes de controladores só são verificados as estruturas.
+     * 
      * @covers StudentController::annualSummary
      *
      * @return void
@@ -41,18 +46,53 @@ class StudentControllerTest extends TestCase
         Artisan::call('db:seed',[
                 '--class' => 'SchoolCalendar2016'
             ]);
+       
+       $subject_attributes = array_keys(factory(Subject::class)->make()->toArray());
 
         $this->get("api/students/1/annual-summary".
-            "?school_calendar_id=1&school_calendar_phase_id=2",
-            $this->getAutHeader())->dump()
-            ->assertJson([
-                "absences_school_calendar_phase" => 4,
-                "absences_school_calendar" => 12,
-                "best_average_school_calendar_phase" => 9.5,
-                "worst_average_school_calendar_phase" => 2.5,
-                "best_average_school_calendar" => 9.7,
-                "worst_average_school_calendar" => 2.3,
-            ]);
+            "?school_calendar_id=1&school_calendar_phase_id=1",
+            $this->getAutHeader())
+            ->seeJsonStructure([
+                "absences_year",
+                'absences_year_phase',
+                "best_average_year" => [
+                    'average', 
+                    'subject' => $subject_attributes
+                ],
+                "low_average_year" => [
+                    'average', 
+                    'subject' => $subject_attributes
+                ],
+                "best_average_year_phase" => [
+                    'average', 
+                    'subject' => $subject_attributes
+                ],
+                "low_average_year_phase" => [
+                    'average', 
+                    'subject' => $subject_attributes
+                ],
+            ])
+            ->seeJsonEquals([
+                "absences_year" => 15,
+                'absences_year_phase' => 4,
+                "best_average_year" => [
+                    'average' => 10,
+                    'subject' => $subject->toArray()
+                ],
+                "low_average_year" => [
+                    'average' => 0.2,
+                    'subject' => $subject2->toArray()
+                ],
+                "best_average_year_phase" => [
+                    'average' => 10,
+                    'subject' => $subject->toArray()
+                ],
+                "low_average_year_phase" => [
+                    'average' => 0.2,
+                    'subject' => $subject2->toArray()
+                ],
+            ])
+            ;
         
     } 
 
