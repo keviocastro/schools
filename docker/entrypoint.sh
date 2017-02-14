7849f379049bcaabbeb41becde7f99087f311bda
Kévio Castro <keviocastro@gmail.com>
2017-02-14 11:37:13 -0200

    Correção de comandos a imagem docker para o ambiente de desenvolvimento

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,8 @@ if [[ ! -f /var/app/vendor/autoload.php ]]; then
 	echo 'File vendor/autoload.php does not exist.'
 	echo 'Running composer install'
 	composer install
+	cp .env.example .env
+	php artisan key:generate
 fi
 
 exec "$@"
