e6d4ebd0b8e9d9e5780c32da681a03ae24deedbc
Kévio Castro <keviocastro@gmail.com>
2017-05-02 16:10:37 -0300

    Migration inicial para avaliações descritivas

--- database/migrations/2017_04_27_164826_descriptive_assessments.php
+++ database/migrations/2017_04_27_164826_descriptive_assessments.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * Epic "Avaliações descritivas e turmas multidisciplinares"
+ * 
+ * @see https://github.com/keviocastro/daily-ui/issues/2
+ * 
+ */
 class DescriptiveAssessments extends Migration
 {
     /**
+     * 
      * Run the migrations.
      *
      * @return void
@@ -17,10 +24,11 @@ class DescriptiveAssessments extends Migration
         Schema::create('evaluation_sheets', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+
             /**
-             * options_per_item:
+             * options:
              * Para armazenar um estrutura fixa de respostas por item de avaliação
-             * 
+             *
              * Exemplo: [
              *      {"identifier":"I", "label": "Irregular"},
              *      {"identifier":"R", "label": "Regular"},
@@ -32,24 +40,41 @@ class DescriptiveAssessments extends Migration
             $table->timestamps();
         });
 
-        // Item de avaliação
-        Schema::create('evaluation_sheet_itens', function (Blueprint $table) {
+
+        Schema::create('evaluation_groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('evaluation_sheet_id');
             $table->string('name');
+            $table->timestamps();
+        });
+
+        // Item de avaliação
+        Schema::create('evaluation_sheet_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('evaluation_sheet_id');
+            $table->unsignedInteger('evaluation_group_id')->nullable();
 
             $table->foreign('evaluation_sheet_id')
                 ->references('id')
                 ->on('evaluation_sheets');
+
+            $table->foreign('evaluation_group_id')
+                ->references('id')
+                ->on('evaluation_groups');
         });
 
-        Schema::create('evaluation_sheet_iten_student', function (Blueprint $table) {
+
+        Schema::create('evaluation_sheet_item_student', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('student_id');
 
             // Identifier de evaluation_sheets.options
             $table->string('option_identifier');
             $table->timestamps();
+
+            $table->foreign('student_id')
+                ->references('id')
+                ->on('students');
         });
 
     }
@@ -61,6 +86,9 @@ class DescriptiveAssessments extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('evaluation_sheet_item_student');
+        Schema::drop('evaluation_sheet_items');
+        Schema::drop('evaluation_groups');
+        Schema::drop('evaluation_sheets');
     }
 }
